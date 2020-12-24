e1a046308bc3a39c0a3e4249ec8deae0459a8fdc
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-12-24 12:56:38 -0500

    🗄️  Seeded database

---
+++ src/database/Schedule.js
@@ -0,0 +1,560 @@
+const Schedule = [
+  // kickoff
+  // Dec 12 -- 26
+  // Dec 13 -- 20
+  {
+    date: 'Dec 12 2020',
+    time: '08:55 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/1',
+    title: 'Kickoff',
+    speakers: {
+      name: 'Sam Hart',
+      title: 'Interchain Foundation',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '09:00 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/2',
+    title: 'Fireside Chat: The State of the Cosmos',
+    speakers: [
+      {
+        name: 'Ethan Buchman',
+        title: 'Co-Founder of Cosmos / CEO of Information Systems',
+      },
+      {
+        name: 'Billy Rennekamp',
+        title: 'Grants Manager at the Interchain Foundation',
+      }
+    ]
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '09:30 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/3',
+    title: 'Towards Tendermint Core 1.0',
+    speakers: {
+      name: 'Tess Rinearson',
+      title: 'VP of Engineering at Interchain GmbH',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '10:00 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/4',
+    title: 'The Road to a Cosmos SDK v1.0',
+    speakers: {
+      name: 'Aaron Craelius',
+      title: 'CTO at Regen Network',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '10:30 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/5',
+    title: 'A Million Blockchains',
+    speakers: {
+      name: 'Peng Zhong',
+      title: 'CEO at Tendermint',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '11:00 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/6',
+    title: 'Regulated DeFi : Stifling innovation or opportunity?',
+    speakers: {
+      name: 'Martin Worner',
+      title: 'CEO at Confio',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '11:30 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/7',
+    title: 'Ethermint & IBC: Bringing the EVM to Cosmos',
+    speakers: {
+      name: 'Federico Kunze Küllmer',
+      title: 'Engineer at Chainsafe',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '12:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/8',
+    title: 'Time preference, IBC & the Era of Coordination',
+    speakers: {
+      name: 'Sean Braithwaite',
+      title: 'CTO at Informal',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '12:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/9',
+    title: 'Model-based Quality Assurance of Tendermint and IBC Implementations in Rust & Go',
+    speakers: {
+      name: 'Andrey Kuprianov',
+      title: 'Senior Research Engineer at Informal',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '01:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/10',
+    title: 'Break',
+    speakers: {
+      name: 'https://www.crowdcast.io/e/interchain-conversations-II/1',
+      title: 'https://www.crowdcast.io/e/interchain-conversations-II/1',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '01:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/11',
+    title: 'Seeking A More Reliable Cosmos: Auditing with Formal Verification',
+    speakers: {
+      name: 'Zarko Milosevic',
+      title: 'Chief Scientist at Informal Systems',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '02:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/12',
+    title: 'A Vision for ABCI++',
+    speakers: {
+      name: 'Dev Ojha',
+      title: 'Co-Founder of Sikka',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '02:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/13',
+    title: 'Threshold Decrypted Transactions: Thwarting Front-Running and Enabling Privacy at the Protocol Level',
+    speakers: {
+      name: 'Sunny Aggarwal',
+      title: 'Co-Founder of Sikka',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '03:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/14',
+    title: 'From Operating to Investing: The Next Evolution of Company Management',
+    speakers: {
+      name: 'Arianne Flemming',
+      title: 'COO at Informal Systems',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '03:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/15',
+    title: 'Options for Achieving Interoperability',
+    speakers: {
+      name: 'Charlie Noyes',
+      title: 'Co-Founder of Paradigm',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '04:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/16',
+    title: 'Bridging CosmosSDK Chains with Ethereum Mainnet',
+    speakers: {
+      name: 'Rick Dudley',
+      title: 'Founder & CEO of Vulcanize ',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '04:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/17',
+    title: 'Peggy: Bridges vs Interoperability',
+    speakers: {
+      name: 'Deborah Simpier',
+      title: 'COO at Althea',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '05:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/18',
+    title: 'It\'s 255:19AM. Do you know what your validation criteria are?',
+    speakers: {
+      name: 'Henry de Valence',
+      title: 'Cryptographer at Zcash',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '05:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/19',
+    title: 'Building a Celo Bridge to Cosmos',
+    speakers: {
+      name: 'James Preswich',
+      title: 'Co-Founder of Summa (Acquired by Celo)',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '06:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/20',
+    title: 'Agoric Public Chain and Economy',
+    speakers: {
+      name: 'Dean Tribble',
+      title: 'CEO at Agoric',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '06:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/21',
+    title: 'Use-cases of IBC—symbiosis between hubs and zones',
+    speakers: {
+      name: 'Do Kwon',
+      title: 'CEO & Co-Founder of Terra',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '07:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/22',
+    title: 'Interchain standards for NFTs+Metadata',
+    speakers: [
+      {
+        name: 'Joe Andrieu',
+        title: 'President, Legendary Requirements',
+      },
+      {
+        name: 'Deepanshu Tripathi',
+        title: 'CTO at Persistence One',
+      }
+    ]
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '07:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/23',
+    title: 'The Role Of End-User Applications In An IBC-Enabled Blockchain Ecosystem',
+    speakers: {
+      name: 'David Park',
+      title: 'CMO at Cosmostation',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '08:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/24',
+    title: 'Why Interchain Services Will Become the "HTTP" of Blockchains',
+    speakers: {
+      name: 'Jeffrey Hu',
+      title: 'Co-Founder of IRISnet',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '08:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/25',
+    title: 'Interchain accounts and Keplr wallet',
+    speakers: {
+      name: 'Josh Lee',
+      title: 'Ecosystem Development at Tendermint',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '09:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/26',
+    title: 'Secure Enclave Signing with the Tendermint KMS',
+    speakers: {
+      name: 'Tomas Tauber',
+      title: 'Researcher at Crypto.com',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '09:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/27',
+    title: 'Decentralized Social Networks and Mass Adoption',
+    speakers: {
+      name: 'Kwun Yeung',
+      title: 'Co-Founder at Forbole',
+    }
+  },
+
+  // -----------------------------------------------------
+
+  {
+    date: 'Dec 12 2020',
+    time: '09:00 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/28',
+    title: 'The ATOM 2021 Initiative',
+    speakers: {
+      name: 'Zaki Manian',
+      title: 'Co-Founder of Iqlusion',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '09:30 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/29',
+    title: 'Inter-blockchain AMM on the Cosmos Hub',
+    speakers: [
+      {
+        name: 'Hyungyeon Lee',
+        title: 'CEO of B-Harvest',
+      },
+      {
+        name: 'Brent Xu',
+        title: 'Head of Strategy & Partnerships at Tendermint',
+      },
+    ]
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '10:00 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/30',
+    title: 'Controlling Frontrunning & MEV: Simulation Results For The Wendy, A Fair Ordering Pre-Protocol',
+    speakers: {
+      name: 'Klaus Kursawe',
+      title: 'Consensus Researcher at Vega Protocol',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '10:30 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/31',
+    title: 'Liquid Staking: Opportunities & Challenges of Staking Derivatives in DeFi',
+    speakers: {
+      name: 'Felix Lutsch',
+      title: 'BD at Chorus One',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '11:00 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/32',
+    title: 'Building Private Smart Contracts with CosmWasm',
+    speakers: {
+      name: 'Guy Zyskind',
+      title: 'Enigma\'s CEO speaking on behalf of the Secret Network',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '11:30 AM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/33',
+    title: 'Private On-Chain Data with Cosmos and Coconut',
+    speakers: {
+      name: 'Dave Hrycyszyn',
+      title: 'Co-Founder of Nym',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '12:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/34',
+    title: 'Fireside Chat on Security, DeFi Composability, & Interoperability',
+    speakers: [
+      {
+        name: 'Stani Kulechov',
+        title: 'CEO of Aave',
+      },
+      {
+        name: 'Julien Bouteloup',
+        title: 'CEO of Stake Capital',
+      },
+      {
+        name: 'Sunny Aggarwal',
+        title: 'Co-Founder of Sikka',
+      },
+    ]
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '12:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/35',
+    title: 'Risk-Adjusted Bonding Curves - System Design and Simulations',
+    speakers: {
+      name: 'Shruti Appiah',
+      title: 'Research Scientist at Block Science',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '01:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/36',
+    title: 'Pluggable Consensus & Data Availability Layer For Cosmos Zones & Rollups',
+    speakers: {
+      name: 'Mustafa Al-Bassam',
+      title: 'CEO & Co-Founder of Lazy Ledger',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '01:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/37',
+    title: 'WalletConnect 2.0: To Interchain and Beyond!',
+    speakers: {
+      name: 'Pedro Gomes',
+      title: 'Founder of WalletConnect',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '02:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/38',
+    title: 'How TLA+ and Apalache Helped Us to Design the Tendermint Light Client',
+    speakers: {
+      name: 'Igor Konnov',
+      title: 'Principal Scientist at Informal Systems',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '02:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/39',
+    title: 'Bitcoin on Cosmos',
+    speakers: {
+      name: 'Matt Bell & Judd Keppel',
+      title: 'Co-Founders of Nomic',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '03:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/40',
+    title: 'Upgradable WASM-based light clients for IBC',
+    speakers: {
+      name: 'Joe Bowman',
+      title: 'Platform & Software Engineering at Chorus One',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '03:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/41',
+    title: 'Governance Working Group: Funding Mechanisms on the Cosmos Hub',
+    speakers: {
+      name: 'Gavin Birch',
+      title: 'Protocol Analyst at Figment',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '04:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/42',
+    title: 'THORChain: Securing Assets With MPC and Incentives',
+    speakers: {
+      name: 'JP Thorbjornsen',
+      title: 'Thorchain',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '04:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/43',
+    title: 'The Future of Radio based FOAM Zones on Cosmos',
+    speakers: {
+      name: 'Ryan John King',
+      title: 'Co-Founder of FOAM',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '05:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/44',
+    title: 'Eradicating Copy/Paste of Crypto-Addresses w/Starname & Deep-Linking',
+    speakers: {
+      name: 'Antoine Herzog',
+      title: 'CEO of Starname',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '05:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/45',
+    title: 'To Ethereum and Beyond: The Future of Interoperability Between the Cosmos Network and External Blockchains',
+    speakers: {
+      name: 'Jazear Brooks',
+      title: 'Founder of Sifchain',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '06:00 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/46',
+    title: 'Why The World Needs A Stateful, Graph-Based Internet of Impact',
+    speakers: {
+      name: 'Shaun Conway',
+      title: 'Founder of IXO',
+    }
+  },
+  {
+    date: 'Dec 12 2020',
+    time: '06:30 PM',
+    timeZone: 'PT',
+    link: 'https://www.crowdcast.io/e/interchain-conversations-II/47',
+    title: 'Applied MEV in the PoS Context & Threshold Decryption As A Solution',
+    speakers: [
+      {
+        name: 'Chjango Unchained & Chris Goes',
+        title: 'Interchain GmbH',
+      },
+      {
+        name: 'Dev Ojha & Sunny Aggarwal',
+        title: 'Sikka Tech',
+      }
+    ]
+  },
+
+]
